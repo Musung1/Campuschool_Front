0030bf7ad20473ab6336861540465c4fb1ca1ddb
musung <musungkim1352@gmail.com>
2024-02-15 21:31:20 +0900

    [Feat] 내 강의실뷰 구현 #9

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import { Paper,styled } from "@mui/material";
 import ClassDetailView from "./views/ClassDetailView";
 import MyOwnClassView from "./views/MyOwnClassView";
 import MyOpenClassView from "./views/MyOpenClassView";
+import MyClassRoomView from "./views/MyClassRoomView";
 function App() {
   const CategoryPaper = styled(Paper)(({ theme }) => ({
     width: 1512,
@@ -17,7 +18,7 @@ function App() {
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
       <CategoryPaper alignItems="center">
         <Header></Header>
-        <MyOpenClassView></MyOpenClassView>
+        <MyClassRoomView></MyClassRoomView>
         <Tail></Tail>
       </CategoryPaper>
     </div>
